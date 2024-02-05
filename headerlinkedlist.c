823c70503aebcc6368d0d9494ae1497ea2a19e0f
Manit S Rao <raomanit273@gmail.com>
2024-02-05 21:55:18 +0530

    display function added too

--- headerlinkedlist.c
+++ headerlinkedlist.c
@@ -71,3 +71,47 @@ NODE deleterear(NODE head)
     head->info-=1;
     return head;
 } 
+void display(NODE head)
+{
+    NODE cur;
+    if(head->info==0)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    cur=head->link;
+    while(cur!=head)
+    {
+        printf("%d\n",head->info);
+        cur=cur->link;
+    }
+}
+void main(){
+    int choice,item;
+    NODE head;
+    head=(NODE)malloc(sizeof(struct node));
+    head->info=0;
+    head->link=head;
+    for(;;){
+        printf("1-Insertfront\t2-Insertrear\n3-Deletefront\t4-Deleterear\n5-Display\tAnyOtherNum-Exit\n");
+        printf("Enter your choice:");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:printf("Enter item to insert:");
+                   scanf("%d",&item);
+                   head=insertfront(head,item);
+                   break;
+            case 2:printf("Enter item to insert:");
+                   scanf("%d",&item);
+                   head=insertrear(head,item);
+                   break;
+            case 3:head=deletefront(head);
+                   break;
+            case 4:head=deleterear(head);
+                   break;
+            case 5:display(head);
+                   break;
+            default:exit(0);
+        }
+    }
+}
